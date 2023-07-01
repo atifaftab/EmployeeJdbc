bc4e2ecc33527b26716bcbd90b1c0d2b69c054c7
Atif <iamatif96@gmail.com>
2023-07-01 18:04:21 +0530

    class to filter employee

--- src/com/amk/assigment/FilterEmployee.java
+++ src/com/amk/assigment/FilterEmployee.java
@@ -1,5 +1,21 @@
 package com.amk.assigment;
 
+import com.amk.model.Employee;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class FilterEmployee {
+
+     List<Employee> list = null;
+
+     public FilterEmployee(){
+          list = new ArrayList<>();
+     }
+
+     public List<Employee> iTEmployees(List<Employee> list){
+          return list.stream().filter(i -> i.getDept().equals("IT")).toList();
+     }
+
 
 }
